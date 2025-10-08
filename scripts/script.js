2fd1aa4ffe7b6a5097f52741c40bed79ddf415f8
Thierry <kouamethierry033@gmail.com>
2025-10-08 14:47:58 +0000

    modification de script js

--- scripts/script.js
+++ scripts/script.js
@@ -57,3 +57,50 @@ async function fetchData(url) {
 btn.addEventListener("click", function () {
   fetchData(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city.value}&lang=fr`);
 });
+
+/* 
+ const currentWeather = {
+        localisation:{
+            pays: data.location.country,
+            ville: data.location.name,
+            region: data.location.region,
+            id_fh: data.location.tz_id,
+            longitude:data.location.lon,
+            latitude:data.location.lat,
+            heure_locale:data.location.localtime,
+            epoch_local:data.location.localtime_epoch,
+        },
+        meteoActuel:{
+          dernier_maj_epoch: data.current.last_updated_epoch,
+          dernier_maj:data.current.last_updated,
+          condition : {
+                text : data.current.condition.text,
+                icon : data.current.condition.icon,
+                code : data.current.condition.code,
+            },
+          temperature:{
+            c: data.current.temp_c,
+            f: data.current.temp_f,
+          },
+          etat_jour:data.current.is_day,
+          vent:{
+            vitesse_mph: data.current.wind_mph,
+            vitesse_kph: data.current.wind_kph,
+            direction: data.current.wind_dir,
+            dir_degree: data.current.wind_degree
+          },
+          humidite:data.current.humidity,
+          nuage: data.current.cloud,
+          uv: data.current.uv,
+          temperature_ressenti:{
+            c: data.current.feelslike_c,
+            f:data.current.feelslike_f
+          },
+          indice_de_chaleur:{
+            c: data.current.heatindex_c ,
+            f:data.current.heatindex_f,
+          }
+
+        }
+    };
+*/
