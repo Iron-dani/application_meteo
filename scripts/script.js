1eb62a0ad6fc1621a40b0e84a1c91b0298f14a04
ASSA DANEIL <danielyoannn@gmail.com>
2025-10-15 13:45:06 +0000

    ajout destructuration,modif html et css

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,74 @@
+/*On procède par destructuration*/
+
+const API_KEY = "266dd225eb1e45aabca125802252609";
+let ville = document.getElementById("texte_ville");
+let btn = document.getElementById("rechercher");
+let affiche = document.getElementById("affiche");
+
+
+async function meteo(url) {
+  try {
+    const firstreponse = await fetch(url);
+    const data = await firstreponse.json();
+
+    /*destructuration*/
+    //const { text } = data.current.condition;
+    const { feelslike_c, humidity, last_updated, precip_mm, temp_c, uv, vis_km } = data.current;
+    //const { ville, pays, region, heure_date, longitude, latitude } = data.location;
+
+    console.log(`Température: ${temp_c}`);
+
+  }catch (error) {
+    console.log('Erreur:', error);
+  }
+}
+
+btn.addEventListener("click", function () {
+  meteo(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${ville.value}&lang=fr`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*const API_KEY = "266dd225eb1e45aabca125802252609"
 const city = "Abidjan"
 async function fetchData(url) {
@@ -34,6 +105,8 @@ btn.addEventListener("click", async function fetchData(url) {
 fetchData(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=fr`);
 */
 
+
+/*bon
 const API_KEY = "266dd225eb1e45aabca125802252609";
 var city = document.getElementById("texte_ville");
 var btn = document.getElementById("rechercher");
@@ -57,50 +130,5 @@ async function fetchData(url) {
 btn.addEventListener("click", function () {
   fetchData(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city.value}&lang=fr`);
 });
-
-/* 
- const currentWeather = {
-        localisation:{
-            pays: data.location.country,
-            ville: data.location.name,
-            region: data.location.region,
-            id_fh: data.location.tz_id,
-            longitude:data.location.lon,
-            latitude:data.location.lat,
-            heure_locale:data.location.localtime,
-            epoch_local:data.location.localtime_epoch,
-        },
-        meteoActuel:{
-          dernier_maj_epoch: data.current.last_updated_epoch,
-          dernier_maj:data.current.last_updated,
-          condition : {
-                text : data.current.condition.text,
-                icon : data.current.condition.icon,
-                code : data.current.condition.code,
-            },
-          temperature:{
-            c: data.current.temp_c,
-            f: data.current.temp_f,
-          },
-          etat_jour:data.current.is_day,
-          vent:{
-            vitesse_mph: data.current.wind_mph,
-            vitesse_kph: data.current.wind_kph,
-            direction: data.current.wind_dir,
-            dir_degree: data.current.wind_degree
-          },
-          humidite:data.current.humidity,
-          nuage: data.current.cloud,
-          uv: data.current.uv,
-          temperature_ressenti:{
-            c: data.current.feelslike_c,
-            f:data.current.feelslike_f
-          },
-          indice_de_chaleur:{
-            c: data.current.heatindex_c ,
-            f:data.current.heatindex_f,
-          }
-
-        }
-    };
 */
+
